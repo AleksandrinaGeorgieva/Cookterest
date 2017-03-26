579390c5405e1f47538cc13c6fb4048073e1979c
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-03-26 19:35:25 +0300

    add edit and delete functionalities

--- config/routes.js
+++ config/routes.js
@@ -16,6 +16,12 @@ module.exports = (app) => {
 
     app.get('/article/details/:id', articleController.details);
 
+    app.get('/article/edit/:id', articleController.editGet);
+    app.post('/article/edit/:id', articleController.editPost);
+
+    app.get('/article/delete/:id', articleController.deleteGet);
+    app.post('/article/delete/:id', articleController.deletePost);
+
     app.get('/user/logout', userController.logout);
 };
 
